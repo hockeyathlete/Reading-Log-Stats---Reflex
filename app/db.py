33c02413381eeda4b214c005d252a9030c2ff1c6
Alan <alan.leeser@gmail.com>
2025-10-14 02:38:09 -0500

    Revert back to .env for database URL

--- app/db.py
+++ app/db.py
@@ -1,14 +1,14 @@
 import reflex as rx
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 from sqlmodel import create_engine
-# import os # Import the os module
+import os # Import the os module
 
 # Retrieve the DATABASE_URL from environment variables
 # Provide a default value for local development if not set, or raise an error if critical
-# load_dotenv()
-# DATABASE_URL = os.getenv("DATABASE_URL")
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine("postgresql+psycopg2://neondb_owner:npg_wTSH6B2oaKGF@ep-blue-silence-ae8a2zpy-pooler.c-2.us-east-2.aws.neon.tech/neondb?sslmode=require&client_encoding=utf8")
+engine = create_engine(DATABASE_URL)
 
 def create_db_and_tables():
     from app.models import SQLModel
