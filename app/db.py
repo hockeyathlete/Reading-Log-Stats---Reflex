ef6e13ef422387772704e7c5ecaa53ea36b8dbec
Alan <alan.leeser@gmail.com>
2025-10-14 02:19:38 -0500

    Fix getenv error. Use load_dotenv() to read .env file Add python-dotenv to requirements.txt

--- app/db.py
+++ app/db.py
@@ -1,10 +1,12 @@
 import reflex as rx
+from dotenv import load_dotenv
 from sqlmodel import create_engine
 import os # Import the os module
 
 # Retrieve the DATABASE_URL from environment variables
 # Provide a default value for local development if not set, or raise an error if critical
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+psycopg2://postgres:postgres123@localhost:5432/reading_tracker_reflex")
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
