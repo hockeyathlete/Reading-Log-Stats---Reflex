eb0a214048603ce66ca65acec6681a34ce7fbd6d
Alan <alan.leeser@gmail.com>
2025-10-14 02:33:41 -0500

    Fix database URL from Neon

--- app/db.py
+++ app/db.py
@@ -8,7 +8,7 @@ from sqlmodel import create_engine
 # load_dotenv()
 # DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine("psql 'postgresql://neondb_owner:npg_wTSH6B2oaKGF@ep-blue-silence-ae8a2zpy-pooler.c-2.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require'")
+engine = create_engine("postgresql+psycopg2://neondb_owner:npg_wTSH6B2oaKGF@ep-blue-silence-ae8a2zpy-pooler.c-2.us-east-2.aws.neon.tech/neondb?sslmode=require&client_encoding=utf8")
 
 def create_db_and_tables():
     from app.models import SQLModel
